bafd0004bf367b089e0003c3b7cca5109adf7c30
Jake <jake@jakejunker.com>
2017-01-09 00:51:04 -0600

    Added new files
    Not sure what I want to do with htaccess yet, since the URL is hardcoded in there. Can't really use it for testing, more research on that...

---
+++ src/php/process_mail.php
@@ -0,0 +1,19 @@
+<?php 
+
+function genToken($form)
+{
+    $token = md5(uniqid(microtime(), true));
+    $_SESSION
+}
+
+function validateEmail()
+{
+    
+}
+
+function sendMail()
+{
+    
+}
+
+?>
