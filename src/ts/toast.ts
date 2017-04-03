f62e5949a9325b044e45a63eebd79999e2a40b2c
Junker <junkerjn@ad.oge.com>
2017-04-03 18:04:19 -0500

    Bug fix in config, other small changes

---
+++ src/ts/toast.ts
@@ -0,0 +1,22 @@
+/**
+ * A class representing a toast notification.
+ * A call to `Toast.Create("...")` displays a new toast on the screen.
+ */
+class Toast
+{
+// Static fields ==================================================================================
+    
+    static _activeToasts: Toast[] = [];
+
+    static Create(msg: string)
+    {
+
+    }
+
+// Instance fields ================================================================================
+
+    private constructor()
+    {
+
+    }
+}
